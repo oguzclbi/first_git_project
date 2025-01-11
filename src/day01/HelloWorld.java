be97f61668f043d80cf3ba0ae8ba55b641a0f376
oguzclbi <156539320+oguzclbi@users.noreply.github.com>
2025-01-11 23:00:16 +0300

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,8 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Java");
+        System.out.println("Hello Programming");
+        System.out.println("Hello Language");
     }
 }
